4f556f2fa3a1e28d25e4c9f6e610fb5d7783a40e
nbkm8y5 <moreno.ronline@gmail.com>
2015-03-17 22:55:57 -0400

    Proper encapsulation
    Another PHP example

--- php/user.php
+++ php/user.php
@@ -1,30 +1,55 @@
 <?php
+
 class User
 {
-
+    /**
+     * @var float
+     */
     private $weight = 0.0;
+    /**
+     * @var float
+     */
     private $heightInInches = 0.0;
+    /**
+     * @var float
+     */
     private $finalNumber = 0.0;
+    /**
+     * @var int
+     */
     private $CALCULATION = 703;
 
+    /**
+     * @param $weight
+     */
     public function setWeight($weight)
     {
         $this->weight = $weight;
     }
 
+    /**
+     * @param $heightInInches
+     */
     public function setHeight($heightInInches)
     {
         $this->heightInInches = $heightInInches;
     }
 
+    /**
+     *
+     */
     private function BMICalculator()
     {
         $BMI = ($this->weight / ($this->heightInInches * $this->heightInInches)) * $this->CALCULATION;
         $this->finalNumber = number_format($BMI, 2);
-        
+
     }
 
-    public function getFinalNumber(){
+    /**
+     * @return float
+     */
+    public function getFinalNumber()
+    {
         $this->BMICalculator();
         return $this->finalNumber;
     }
